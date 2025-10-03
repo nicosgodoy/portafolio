62bf3c3e6dd1fee959d2a7ccc0ea2e57c5cd7826
nicolas Godoy <nicodepor3@gmail.com>
2025-10-02 22:58:09 -0300

    cambio de los archivos

--- portafolio/astro.config.mjs
+++ portafolio/astro.config.mjs
@@ -1,5 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+//import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+//export default defineConfig({});
+
+
+
+// @ts-check
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react"; // ⬅️ Necesario si usas React
+
+// https://astro.build/config
+export default defineConfig({
+  // ✅ CRUCIAL: MODO ESTATICO. Esto resuelve el problema del despliegue en Netlify.
+  output: 'static', 
+  
+  // ✅ Incluye React si lo usas para componentes de interfaz.
+  integrations: [
+    react()
+  ],
+  
+  // ❌ No se incluye ningún adaptador (netlify, vercel)
+});
+
